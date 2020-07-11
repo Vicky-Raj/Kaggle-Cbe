08e1ffd3017125d13e957875f17d284139d56b09
Vishnu Varthan <vishnulatha006@gmail.com>
2020-07-11 20:50:08 +0530

    Completed Contact Page

--- cbe_app/models.py
+++ cbe_app/models.py
@@ -26,3 +26,10 @@ class Team(models.Model):
 
     def __str__(self):
         return self.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message =models.TextField()
+    def __str__(self):
+        return self.name
--- cbe_app/views.py
+++ cbe_app/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.views.generic import View
-from .models import Event,Team
+from .models import Event, Team, Contact
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+import json
+
 
 class HomeView(View):
     template_name = 'cbe_app/index.html'
-    def get(self,request):
+
+    def get(self, request):
         events = Event.objects.all()
-        teams= Team.objects.all()
-        return render(request,self.template_name ,{"events":events,"teams":teams})
+        teams = Team.objects.all()
+        return render(request, self.template_name, {"events": events, "teams": teams})
+
+
+class ContactView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        name = data['name']
+        email = data['email']
+        message = data['message']
+        exists = Contact.objects.filter(email=email).exists()
+        if (exists):
+            return JsonResponse({"added": False})
+        else:
+            contact = Contact()
+            contact.email = email
+            contact.message = message
+            contact.name =name
+            contact.save()
+            return JsonResponse({"added": True})
+        return JsonResponse({})
